c1c2adc26869f11b5a22701f8b3a33273efeaeda
chad kidd <ckiddwebdev@gmail.com>
2019-04-23 15:18:51 -0700

    GET posts by id working

--- data/post-router.js
+++ data/post-router.js
@@ -16,6 +16,24 @@ router.get('/', async (req, res) => {
     }
 })
 
+router.get('/:id', async (req, res) => {
+    try {
+     const postbyid = await PostData.findById(req.params.id);
+    
+     if (postbyid) {
+         res.status(200).json(postbyid)
+     } else {
+         res.status(404).json({
+            message: "The post with the specified ID does not exist."
+         })
+     }
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+          error: "The post information could not be retrieved."
+        }); }
+})
+
 
 
 module.exports = router;
